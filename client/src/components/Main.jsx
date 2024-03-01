6805ab4d8d537225f70fce15ac1807295c4b48e8
Josh Lazar <lazarjosh2@gmail.com>
2024-03-01 11:14:48 -0500

    added disclaimer regarding authentication features

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -23,6 +23,7 @@ const Main = () => {
                 <div className='bg-white rounded-xl flex flex-col p-2 justify-center items-center space-y-2'>
                     <p className='text-xl font-bold text-blue-500'>Shopiyo</p>
                     <p className='text-lg text-slate-900 text-center'>Full CRUD MERN Stack E-commerce App Utilizing Redux, Tailwind, Multer For File Uploads to Cloudinary API to Host</p>
+                    <p className='text-lg text-slate-900 text-center'>App is hosted on render which uses a public suffix creating issues with storing cookies. All features work on chrome on desktop. You can test responsiveness on mobile but authentication features may not work.</p>
                     <div className='space-x-3'>
                         <button onClick={() => window.open('https://shopiyo-site.onrender.com/', '_blank')} className='bg-blue-500 text-white p-3 rounded-xl hover:shadow-2xl hover:bg-blue-600'>Visit App</button>
                         <button onClick={() => window.open('https://github.com/joshlazar2/e-commerce-cloudinary', '_blank')} className='bg-blue-500 text-white p-3 rounded-xl hover:shadow-2xl hover:bg-blue-600'>View Source Code</button>
